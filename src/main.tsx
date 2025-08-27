aaa266d401c03829f5a8a2d2b43851947e9a3209
MohamedAli <mmkandeelz74@gmail.com>
2025-08-28 02:35:33 +0300

    refactor(router): convert BrowserRouter + Routes to createBrowserRouter with RouterProvider for modern routing setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./app/layout.tsx";
-import HomePAge from "./app/home/page.tsx";
+import HomePage from "./app/home/page.tsx";
+
+// Routers
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<RootLayout />}>
-          <Route index element={<HomePAge />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
