551a869d24b4a84f5348eda491b27526cfa27eb5
Ahmed-Qotb <bl7ax99fg@gmail.com>
2025-08-28 17:40:28 +0300

    chore(auth-layout-kyc)

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,21 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RootLayout from "./app/layout.tsx";
 import HomePAge from "./app/home/page.tsx";
+import AuthLayout from "./app/auth/layout.tsx";
+import KYC from "./app/auth/KYC/page.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        {/* Home page layout */}
         <Route element={<RootLayout />}>
           <Route index element={<HomePAge />} />
+        </Route>
+
+        {/* Auth pages layout */}
+        <Route element={<AuthLayout />}>
+          <Route path="/auth/kyc" element={<KYC />} />
         </Route>
       </Routes>
     </BrowserRouter>
