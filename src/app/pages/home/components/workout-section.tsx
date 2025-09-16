44c13d4021186097cb559ab939cf564385ef3960
MohamedAli <mmkandeelz74@gmail.com>
2025-09-16 04:31:46 +0300

    feat(i18n): finish translation for workouts section (EN/AR)

--- src/app/pages/home/components/workout-section.tsx
+++ src/app/pages/home/components/workout-section.tsx
@@ -2,7 +2,7 @@ import { Dumbbell } from "lucide-react";
 import EmblaCarousel from "./workout-carousel";
 import type { EmblaOptionsType } from "embla-carousel";
 import { workouts } from "@/lib/constants/workouts.const";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { useMediaQuery } from "react-responsive";
@@ -13,7 +13,6 @@ function WorkoutSection() {
 
   // States
   const [count, setCount] = useState(3);
-  const [mobileSlides, setMobileSlides] = useState(workoutsSlides.slice(0, count));
 
   // Checks of screen size
   const isMobile = useMediaQuery({ maxWidth: 768 });
@@ -29,33 +28,38 @@ function WorkoutSection() {
 
   return (
     <section className="w-full lg:h-screen bg-[url('/assets/traidmails.jpg')] bg-cover bg-center py-6">
-      <span className="relative flex lg:items-center text-main font-semibold text-sm lg:justify-center before:content-['WORKOUTS'] before:absolute before:hidden before:text-[64px] before:font-bold before:text-white/10 lg:before:flex before:justify-center before:bottom-4 items-start before:left-1/2 before:-translate-x-1/2 before:whitespace-nowrap mt-5">
-        <Dumbbell fill="orange" width={34} height={20} className="text-main rotate-45" />
+      <span className="relative flex lg:items-center text-main font-semibold text-sm lg:justify-center before:content-['WORKOUTS'] rtl:before:content-['التمارين'] before:absolute before:hidden before:text-[64px] before:font-bold before:text-white/10 lg:before:flex before:justify-center before:bottom-4 rtl:before:bottom-8 items-start before:left-1/2 before:-translate-x-1/2 before:whitespace-nowrap mt-5">
+        <Dumbbell fill="orange" width={34} height={20} className="text-main rotate-45 w-9" />
         {t("fitness-class")}
       </span>
       <div className="h-full lg:h-[380px] lg:bg-grayExtra/70 dark:lg:bg-charcoal/70 lg:backdrop-blur-3xl flex flex-col lg:items-center items-start gap-5">
-        <h2 className="font-bold text-[40px] text-charcoal dark:text-grayExtra px-2 max-w-[600px] text-wrap">
-          Transform Your Body with Our Dynamic <span className="text-main">Upcoming Workouts</span>
+        <h2 className="font-bold text-[40px] lg:text-charcoal text-grayExtra dark:text-grayExtra px-2 max-w-[600px] text-wrap">
+          <Trans
+            i18nKey="workouts.heading"
+            components={{ strong: <span className="text-main" /> }}
+          />
         </h2>
+
         {/*Muscles take from merge */}
         {/* <MuscleGroup /> */}
 
         {/* Carousel */}
         <section className="w-full ">
-          <EmblaCarousel slides={isMobile ? mobileSlides : workoutsSlides} options={OPTIONS} />
+          <EmblaCarousel
+            slides={isMobile ? workoutsSlides.slice(0, count) : workoutsSlides}
+            options={OPTIONS}
+          />
           {count < workoutsSlides.length && isMobile && (
             <div className="text-end container">
               <Button
                 className="text-main bg-transparent hover:bg-transparent"
                 onClick={() => {
                   if (count + 3 <= workoutsSlides.length) {
-                    const newCount = count + 3;
-                    setCount(newCount);
-                    setMobileSlides(workoutsSlides.slice(0, newCount));
+                    setCount(count + 3);
                   }
                 }}
               >
-                See More
+                {t("see-more")}
               </Button>
             </div>
           )}{" "}
