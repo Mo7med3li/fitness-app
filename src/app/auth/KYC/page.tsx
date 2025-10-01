ca3e89334159f3053b6fd1cc81a990fd93ccd8bf
MahmoudSayedAbdi <mahmoudsayed.2003.8@gmail.com>
2025-10-01 16:51:20 +0300

    fix(bugs)

--- src/app/auth/KYC/page.tsx
+++ src/app/auth/KYC/page.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { StepRadialProgress } from "@/components/common/chart-radial";
-import KycForm from "./components/kyc-form";
+// import KycForm from "./components/kyc-form";
 import { useTranslation } from "react-i18next";
 
 export default function Page() {
@@ -8,7 +8,7 @@ export default function Page() {
   const { t } = useTranslation();
 
   // State
-  const [step, setStep] = useState(1);
+  const [step /*setStep*/] = useState(1);
 
   return (
     <div className="flex flex-col justify-center items-center w-full px-4">
@@ -22,31 +22,29 @@ export default function Page() {
         {/* Step 1 Gender */}
         {step === 1 && t("auth-heading-gender")}
         {/* Step 2 Age */}
-        {step === 2 && t('auth-header-age')}
+        {step === 2 && t("auth-header-age")}
         {/* Step 3 weight */}
-        {step === 3 && t('auth-header-weight')}
+        {step === 3 && t("auth-header-weight")}
         {/* Step 4 Height*/}
-        {step === 4 && t('auth-header-height')}
+        {step === 4 && t("auth-header-height")}
         {/* Step 5 goal*/}
-        {step === 5 && t('auth-header-goal')}
+        {step === 5 && t("auth-header-goal")}
         {/* Step 6  activity level*/}
         {step === 6 && (
           <>
-            {t('your-regular-physical')}
-            <br /> {t('activity-level')}
+            {t("your-regular-physical")}
+            <br /> {t("activity-level")}
           </>
         )}
       </h2>
 
       {/* Description */}
       <p className="text-center mb-8 capitalize">
-        {step === 1
-          ? t('gender-descreption')
-          : t('rest-auth-descreption')}
+        {step === 1 ? t("gender-descreption") : t("rest-auth-descreption")}
       </p>
 
       {/* Form */}
-      <KycForm step={step} setStep={setStep} />
+      {/* <KycForm step={step} setStep={setStep} /> */}
     </div>
   );
 }
